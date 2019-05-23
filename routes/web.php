e014fae80760673c17ab6d3b0c6087cddea9d427
iarafat <miyasinarafat@gmail.com>
2019-05-23 17:06:54 +0600

    trending articles with sorted sets: lesson 3

--- routes/web.php
+++ routes/web.php
@@ -12,3 +12,29 @@ Route::get('/videos/{id}/download', function ($id) {
 
     return back();
 });
+
+Route::get('articles/trending', function () {
+    $trending = Redis::zrevrange('trending_articles', 0, 2);
+
+    $trending = App\Article::hydrate(
+        array_map('json_decode', $trending)
+    );
+
+    dd($trending);
+
+    return $trending;
+});
+
+Route::get('articles/{article}', function (App\Article $article) {
+    Redis::zincrby('trending_articles', 1, $article);
+
+    // remove articles from redis that not trending by cron jobs with console command or schedule
+    // Redis::zremrangebyrank('trending_articles', 0, -101);
+
+    return $article;
+});
+
+
+
+
+

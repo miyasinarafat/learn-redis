581ba87be4fac06cbec21a67a10bd65d8374c593
iarafat <miyasinarafat@gmail.com>
2019-05-26 11:34:20 +0600

    refactoring setps three, caching with redis:lesson 5

--- routes/web.php
+++ routes/web.php
@@ -1,20 +1,9 @@
 <?php
 
 use Illuminate\Support\Facades\Cache;
-use Illuminate\Support\Facades\Redis;
-
-
-function remember($key, $minuets, $callback) {
-    if ($value = Redis::get($key))
-        return unserialize($value);
-
-    Redis::setex($key, $minuets, $value = serialize($callback()));
-
-    return $value;
-}
 
 Route::get('/', function () {
-    return remember('articles.all', 60 * 60, function () {
+    return Cache::remember('articles.all', 60 * 60, function () {
        return \App\Article::all();
     });
 });
